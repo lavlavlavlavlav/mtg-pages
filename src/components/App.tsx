9ed557728ff7e4d1544d45a72a533bed09995e20
Lavlavlavlavlav <timmi777@hotmail.de>
2024-10-03 18:16:59 +0200

    base layout

---
+++ src/components/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './App.css';
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable';
+
+function App() {
+  return (
+    <>
+      <div className="h-screen w-full overflow-hidden">
+        <ResizablePanelGroup direction="horizontal" className="h-full w-full">
+          <ResizablePanel defaultSize={15}>
+            <div className="flex h-full items-center justify-center">
+              <span className="font-semibold">Sidebar</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={85}>
+            <div className="flex h-full items-center justify-center">
+              <span className="font-semibold">Content</span>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </div>
+    </>
+  );
+}
+
+export default App;
