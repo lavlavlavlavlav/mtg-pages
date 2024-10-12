bbaac8a7f9ef572ceb95cb3ee7ebad60892420c2
Lavlavlavlavlav <timmi777@hotmail.de>
2024-10-12 13:15:01 +0200

    obfuscate stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/mtg-pages/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    obfuscatorPlugin({
+      options: {
+        debugProtection: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
